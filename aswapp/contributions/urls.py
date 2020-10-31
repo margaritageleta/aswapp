be3bd66b70d6dc24649bbd15d52f3e519ff8c72b
Rita <rita.geleta@jediupc.com>
2020-10-31 20:42:17 +0100

    Add url + classification

--- aswapp/contributions/urls.py
+++ aswapp/contributions/urls.py
@@ -3,8 +3,8 @@ from contributions import views
 
 
 urlpatterns = [
-    url(r'news', views.news_view, name='News view'),
-    url(r'newest', views.newest_view, name='Newest view'),
-    url(r'submit/', views.SubmitView.as_view(), name='submit_contribution')
-   
+    url(r'news', views.news_view, name='news_view'),
+    url(r'newest', views.newest_view, name='newest_view'),
+    url(r'submit/', views.SubmitView.as_view(), name='submit_contribution'),
+    url(r'^(?P<kind>[-\w]+)/(?P<id>[0-9A-Za-z_\-]+)/$', views.show_contribution_view, name='show_contribution_view'),
 ]
