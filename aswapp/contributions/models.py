95794643154e90d0731a0f7c232c3b4eac549011
Adria <adria.de.castro@estudiantat.upc.edu>
2020-11-12 15:19:24 +0100

    Heroku

--- aswapp/contributions/models.py
+++ aswapp/contributions/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.apps import apps
-from users.models import User
+#from users.models import User
 
 
 # Create your models here.
