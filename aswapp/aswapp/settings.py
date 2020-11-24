7dddee0d2e34a78cce7d19039c25a7162c5ccd2d
Adria <adria.de.castro@estudiantat.upc.edu>
2020-11-25 00:13:02 +0100

    Heroku works?

--- aswapp/aswapp/settings.py
+++ aswapp/aswapp/settings.py
@@ -134,8 +134,8 @@ else:
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
