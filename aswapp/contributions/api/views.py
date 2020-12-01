5fced6a5e19df3c6be43f599bc40134e524fad6d
Rita <rita.geleta@jediupc.com>
2020-12-01 20:01:05 +0100

    Delete API done, PATCH under question

--- aswapp/contributions/api/views.py
+++ aswapp/contributions/api/views.py
@@ -10,33 +10,33 @@ from contributions.api.serializers import PublicationSerializer
 class ItemsListAPIView(ListAPIView):
     queryset = ''
     serializer_class = PublicationSerializer
-
+    # Get all publications (all kinds)
     def get(self, request): 
         queryset = Publication.objects.all()
         serializer_class = PublicationSerializer(queryset, many=True)
         return Response(serializer_class.data, status=status.HTTP_200_OK)
-    
-    def item_existing(self, url): 
-        return Publication.objects.filter(url=url).count()
-
+    # Create a new publication
     def post(self,request):
         serializer_class = PublicationSerializer(data=request.data)
+        # If form data is valid (all params set)
         if serializer_class.is_valid():
+            # If the url is repeated
             if request.data['kind'] == '1' and Publication.objects.filter(url=request.data['url']).exists():
                 return Response({'status': 'Error 409, url already exists'}, status=status.HTTP_409_CONFLICT)
+            # Otherwise a completely new publication
             else:
                 serializer_class.save()
-                # CommentAPI.post() <-- hipotesis POST 201
+                # TODO CommentAPI.post() <-- hipotesis POST 201
                 return Response(serializer_class.data, status=status.HTTP_201_CREATED)  
         else:
             return Response({'status': 'Error 400, bad request'}, status=status.HTTP_400_BAD_REQUEST)
-        # TODO 401
+        # TODO 401 authorization to create an item
         # TODO Look up the creation of asks -> not comment associated
 
 class ItemsAsksListAPIView(ListAPIView):
     queryset = ''
     serializer_class = PublicationSerializer
-
+    # Get all publications of type ASK
     def get(self, request): 
         queryset = Publication.objects.filter(kind=0).all()
         serializer_class = PublicationSerializer(queryset, many=True)
@@ -45,7 +45,7 @@ class ItemsAsksListAPIView(ListAPIView):
 class ItemUrlsListAPIView(ListAPIView):
     queryset = ''
     serializer_class = PublicationSerializer
-
+    # Get all publications of type URL
     def get(self, request): 
         queryset = Publication.objects.filter(kind=1).all()
         serializer_class = PublicationSerializer(queryset, many=True)
@@ -54,16 +54,45 @@ class ItemUrlsListAPIView(ListAPIView):
 class ItemAPIView(ListAPIView):
     queryset = ''
     serializer_class = PublicationSerializer
-
+    # Get an item by id
     def get(self, request, id, format=None):
         queryset = Publication.objects.filter(id=id).first()
         serializer_class = PublicationSerializer(queryset, many=False)
+        # If items exists, return JSON
         if queryset:
             return Response(serializer_class.data, status=status.HTTP_200_OK)
+        # Otherwise, it does not exist, return error
         else:
             return Response({'status': 'Error 404, item not found'}, status=status.HTTP_404_NOT_FOUND)
+    # Delete an item by id
+    def delete(self, request, id, format=None):
+        queryset = Publication.objects.filter(id=id).first()
+        # On successful delete, return no content
+        if queryset:
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        # Otherwise return error
+        else:
+            return Response({'status': 'Error 404, item not found'}, status=status.HTTP_404_NOT_FOUND)
+        # TODO 403 forbidden to delete not yours
+        # TODO 401 authorization to delete yours
+    
+    # TODO How to update votes?
+    """
+    def patch(self, request, id, format=None):
+        queryset = Publication.objects.filter(id=id).first()
+        serializer_class = PublicationSerializer(queryset, many=False)
+        # On successful delete, return no content
+        if queryset and serializer_class.is_valid():
+            ...
+        # Otherwise return error
+        else:
+            return Response({'status': 'Error 404, item not found'}, status=status.HTTP_404_NOT_FOUND)
+        # TODO 403 forbidden to delete not yours
+        # TODO 401 authorization to delete yours
+    """
 
 class ItemCommentsListAPIView(ListAPIView):
     pass
+
 class CommentAPIView(ListAPIView):
     pass
