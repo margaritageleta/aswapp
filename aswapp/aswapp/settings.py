4875d75a562569931ccbfaabbe13ecc8e6dbf485
Adria <adria.de.castro@estudiantat.upc.edu>
2020-12-02 16:11:55 +0100

    New libraries

--- aswapp/aswapp/settings.py
+++ aswapp/aswapp/settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
     'social_django',
     # third party
     'rest_framework',
+    'oauth2_provider',
     'rest_framework_social_oauth2',
     'rest_framework_api_key',
     # own
