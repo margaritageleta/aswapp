e8c5e736a8248c8f6b53a4fd70aaff4c6ea6ee91
Toni <garcia.calvin.toni@gmail.com>
2020-12-02 17:59:52 +0100

    Voted Items and Voted comments

--- aswapp/users/api/views.py
+++ aswapp/users/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
-from contributions.models import Publication, Comment
+from contributions.models import Publication, Comment, VoteComment, VotePublication
 from contributions.api.serializers import PublicationSerializer, CommentSerializer
 from users.models import Hacker
 from users.api.serializers import HackerSerializer
@@ -55,19 +55,56 @@ class UserCommentsListAPIView(ListAPIView):
 
 class UserVotedItemsListAPIView(ListAPIView):
     queryset = ''
+    serializer_class = PublicationSerializer
+    # Get an user by id
+    def get(self, request, id, format=None):
+        author = Hacker.objects.get(id=id)
+               
+        # If there are voted publications, return JSON
+        if VotePublication.objects.filter(voter=author).exists():
+            data = []
+            for vp in VotePublication.objects.filter(voter=author).all():
+                data.append({
+                    'id': vp.contribution.id,
+                    'author': vp.contribution.author,
+                    'created_at': vp.contribution.created_at,
+                    'number_votes': vp.contribution.number_votes,
+                    'title': vp.contribution.title,
+                    'question': vp.contribution.question,
+                    'url': vp.contribution.url,
+                    'kind': vp.contribution.kind 
+                })
+
+            queryset = data
+            serializer_class = PublicationSerializer(queryset, many=True)
+            return Response(serializer_class.data, status=status.HTTP_200_OK)
+        # Otherwise, it does not exist, return error
+        else:
+            return Response({'status': 'Error 404, user does not has any voted publications'}, status=status.HTTP_404_NOT_FOUND)
+
+class UserVotedCommentsListAPIView(ListAPIView):
+    queryset = ''
     serializer_class = CommentSerializer
     # Get an user by id
     def get(self, request, id, format=None):
         author = Hacker.objects.get(id=id)
                
-        # If there are publications, return JSON
-        if Comment.objects.filter(author=author).exists():
-            queryset = Comment.objects.filter(author=author).all()
+        # If there are voted publications, return JSON
+        if VoteComment.objects.filter(voter=author).exists():
+            data = []
+            for vp in VoteComment.objects.filter(voter=author).all():
+                data.append({
+                    'id': vp.contribution.id,
+                    'author': vp.contribution.author,
+                    'comment': vp.contribution.comment,
+                    'created_at': vp.contribution.created_at,
+                    'referenced_publication': vp.contribution.referenced_publication,
+                    'parent': vp.contribution.parent, 
+                })
+
+            queryset = data
             serializer_class = CommentSerializer(queryset, many=True)
             return Response(serializer_class.data, status=status.HTTP_200_OK)
         # Otherwise, it does not exist, return error
         else:
-            return Response({'status': 'Error 404, user does not has any comments'}, status=status.HTTP_404_NOT_FOUND)
-
-class UserVotedCommentsListAPIView(ListAPIView):
-    pass
+            return Response({'status': 'Error 404, user does not has any voted comments'}, status=status.HTTP_404_NOT_FOUND)
